88b71500ffdcf4194b93c6e57f9441914ec06f22
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-12-11 15:24:16 +0700

    feat(pages): integrate components into landing page

---
+++ client/src/app/(pages)/page.tsx
@@ -0,0 +1,12 @@
+import { MainLayout } from '@/components/layouts';
+import { LandingCTA } from '@/components/organisms';
+
+const Home = () => {
+  return (
+    <MainLayout>
+      <LandingCTA />
+    </MainLayout>
+  );
+};
+
+export default Home;
