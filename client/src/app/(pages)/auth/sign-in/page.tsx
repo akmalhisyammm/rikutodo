424f672149a281c3e24c9eb30f41715142968509
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-12-11 15:49:33 +0700

    feat(pages): integrate components into auth pages

---
+++ client/src/app/(pages)/auth/sign-in/page.tsx
@@ -0,0 +1,12 @@
+import { AuthLayout } from '@/components/layouts';
+import { SignInForm } from '@/components/organisms';
+
+const SignIn = () => {
+  return (
+    <AuthLayout>
+      <SignInForm />
+    </AuthLayout>
+  );
+};
+
+export default SignIn;
---
+++ client/src/app/(pages)/auth/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { AuthLayout } from '@/components/layouts';
+import { SignUpForm } from '@/components/organisms';
+
+const SignUp = () => {
+  return (
+    <AuthLayout>
+      <SignUpForm />
+    </AuthLayout>
+  );
+};
+
+export default SignUp;
